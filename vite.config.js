464e3f136fca1814aaf5b79695a06be219c6349c
Peter Nguyen <peternguyen.officiel@gmail.com>
2026-06-25 22:26:57 -0400

    Fix gh_pages bugs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Must match the GitHub repo name for project pages: https://<user>.github.io/<repo>/
+  base: '/TNT_DemoApp/',
   plugins: [react()],
 })
